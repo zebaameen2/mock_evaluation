c4f3d95ce2d0b3edcac5c130a4abf4787c3bb9a3
zebaameen2 <tayyabameen276@gmail.com>
2025-11-23 20:00:05 +0530

    ADD: update comment

---
+++ mock_eval_1/Part-B/ques9.js
@@ -0,0 +1,13 @@
+displayCar()
+displayTruck()
+displayBike()
+vehicleInfo(vehicleCategory, callbackFn)
+function displayCar() {
+    console.log("Car: Toyota");
+} 
+function displayTruck() {
+    console.log("Truck: Ford");
+}
+function displayBike() {
+    console.log("Bike: Yamaha");
+}   
